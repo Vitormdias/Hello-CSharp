e596ae559e3639d986f5f1bfd6e9469bce4f1113
vitormdias <vitor.dias@ventron.com.br>
2017-05-22 11:26:40 -0300

    abstract added

---
+++ Todo.API/Todo.Data/Abstract/IRepositories.cs
@@ -0,0 +1,8 @@
+﻿namespace Todo.Data.Abstract
+{
+    public interface ITaskRepository : IEntityBaseRepository<Model.Task> { }
+
+    public interface IMemberRepository : IEntityBaseRepository<Model.Member> { }
+
+    public interface ITeamRepository : IEntityBaseRepository<Model.Team> { }
+}
