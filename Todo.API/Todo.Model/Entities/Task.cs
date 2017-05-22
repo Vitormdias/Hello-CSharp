744d1b6ca22e95d089f69ad3e2f58b49a5e573eb
vitormdias <vitor.dias@ventron.com.br>
2017-05-22 11:26:22 -0300

    field renamed

--- Todo.API/Todo.Model/Entities/Task.cs
+++ Todo.API/Todo.Model/Entities/Task.cs
@@ -7,7 +7,7 @@ namespace Todo.Model
     public class Task : IEntityBase
     {
         public int Id { get; set; }
-        public string Content { get; set; }
+        public string Description { get; set; }
         public Member Owner { get; set; }
         public DateTime Due { get; set; }
         public int Status { get; set; }
