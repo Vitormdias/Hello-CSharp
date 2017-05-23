cf7642a4c3c08f2ba03b1a3929f3b5a90e8c6a20
vitormdias <vitor.dias@ventron.com.br>
2017-05-23 13:06:01 -0300

    startup file updated

--- Todo.API/Todo.API/Startup.cs
+++ Todo.API/Todo.API/Startup.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Todo.Data;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Serialization;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace Todo.API
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+        private static string _applicationPath = string.Empty;
+        string sqlConnectionString = string.Empty;
+        bool useInMemoryProvider = false;
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -19,25 +24,79 @@ namespace Todo.API
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
             Configuration = builder.Build();
         }
-
-        public IConfigurationRoot Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+            string sqlConnectionString = Configuration.GetConnectionString("DefaultConnection");
+            try
+            {
+                useInMemoryProvider = bool.Parse(Configuration["AppSettings:InMemoryProvider"]);
+            }
+            catch { }
+
+            // Repositories
+            services.AddScoped<Todo.Data.Abstract.IMemberRepository, Data.Repositories.MemberRepository>();
+            services.AddScoped<Data.Abstract.ITeamRepository, Data.Repositories.TeamRepository>();
+            services.AddScoped < Data.Abstract.ITaskRepository, Data.Abstract.ITaskRepository>();
+
+            // Automapper Configuration
+            //AutoMapperConfiguration.Configure();
+
+            //Enable Cors
+            services.AddCors();
+
+            services.AddMvc()
+                .AddJsonOptions(opts =>
+                {
+                    // Force Camel Case to JSON
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            //app.UseStaticFiles();
 
-            app.UseMvc();
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+            app.UseExceptionHandler(
+              builder =>
+              {
+                  builder.Run(
+                    async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            //context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
+                        }
+                    });
+              });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+            TodoDbInitializer.Initialize(app.ApplicationServices);
         }
     }
 }
