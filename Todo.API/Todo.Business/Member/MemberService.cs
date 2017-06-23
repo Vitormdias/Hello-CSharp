b422b5947df98d4fd3790b81b4b06a4dac4a49b7
vitormdias <vitor.dias@ventron.com.br>
2017-06-23 17:01:59 -0300

    member service added

---
+++ Todo.API/Todo.Business/Member/MemberService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Model;
+using Todo.Data.Abstract;
+
+namespace Todo.Business.Member
+{
+    public class MemberService
+    {
+        private IMemberRepository memberRepository ;
+        public MemberService(IMemberRepository memberRepository)
+        {
+            this.memberRepository = memberRepository;
+        }
+        public IEnumerable<Model.Member> Get ()
+        {
+            return memberRepository.GetAll();
+        }
+    }
+}
