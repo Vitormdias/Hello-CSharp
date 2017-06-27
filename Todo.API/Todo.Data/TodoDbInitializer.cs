4c3b204ddcf93feff915d350ec22a0f7db81b963
vitormdias <vitormd97@gmail.com>
2017-06-27 20:59:18 -0300

    initializer added

--- Todo.API/Todo.Data/TodoDbInitializer.cs
+++ Todo.API/Todo.Data/TodoDbInitializer.cs
@@ -15,25 +15,31 @@ namespace Todo.Data
         public static void Initialize (IServiceProvider serviceProvider)
         {
             context = (TodoContext)serviceProvider.GetService(typeof(TodoContext));
+
+            InitializeTodo();
         }
 
         private static void InitializeTodo()
         {
+            if (!context.Teams.Any())
+            {
+                Team team_01 = new Team
+                {
+                    Name = "Team 1"
+                };
+
+                context.Teams.Add(team_01);
+
+                context.SaveChanges();
+            }
+
             if (!context.Members.Any())
             {
                 Member member_01 = new Member
                 {
                     Name = "Vitor",
                     Age = 19,
-                    Tasks = new List<Model.Task>()
-                    {
-                        new Model.Task()
-                        {
-                            Description = "Fazer Comida",
-                            Due = DateTime.Today.AddDays(2),
-                            Status = (int)Status.Created
-                        }
-                    }
+                    TeamId = 1
                 };
 
                 context.Members.Add(member_01);
@@ -41,20 +47,19 @@ namespace Todo.Data
                 context.SaveChanges();
             }
 
-            if (!context.Teams.Any())
+            if (!context.Tasks.Any())
             {
-                Team team_01 = new Team
+                Model.Task task_01 = new Model.Task()
                 {
-                    Name = "Team 1",
-                    Members = new List<Member>()
-                    {
-                        new Member
-                        {
-                            Id = 1
-                            
-                        }
-                    }
+                    Description = "Fazer Comida",
+                    Due = DateTime.Today.AddDays(2),
+                    Status = (int)Status.Created,
+                    OwnerId = 1
                 };
+
+                context.Tasks.Add(task_01);
+
+                context.SaveChanges();
             }
         }
     }
