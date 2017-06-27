3cebd18a839d34f131ace85a997af1b5a0f288c7
vitormdias <vitor.dias@ventron.com.br>
2017-06-27 17:28:41 -0300

    initializer started

--- Todo.API/Todo.Data/TodoDbInitializer.cs
+++ Todo.API/Todo.Data/TodoDbInitializer.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Todo.Model;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Todo.Data
 {
@@ -11,6 +15,47 @@ namespace Todo.Data
         public static void Initialize (IServiceProvider serviceProvider)
         {
             context = (TodoContext)serviceProvider.GetService(typeof(TodoContext));
+        }
+
+        private static void InitializeTodo()
+        {
+            if (!context.Members.Any())
+            {
+                Member member_01 = new Member
+                {
+                    Name = "Vitor",
+                    Age = 19,
+                    Tasks = new List<Model.Task>()
+                    {
+                        new Model.Task()
+                        {
+                            Description = "Fazer Comida",
+                            Due = DateTime.Today.AddDays(2),
+                            Status = (int)Status.Created
+                        }
+                    }
+                };
+
+                context.Members.Add(member_01);
+                
+                context.SaveChanges();
+            }
+
+            if (!context.Teams.Any())
+            {
+                Team team_01 = new Team
+                {
+                    Name = "Team 1",
+                    Members = new List<Member>()
+                    {
+                        new Member
+                        {
+                            Id = 1
+                            
+                        }
+                    }
+                };
+            }
         }
     }
 }
