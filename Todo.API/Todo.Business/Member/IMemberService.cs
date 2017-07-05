82bcda1b6693fee19246275ee7b8210e2981e276
Medson de Oliveira Junior <medson.oliveira@fatec.sp.gov.br>
2017-07-05 03:31:24 -0300

    Add Interface IMemberService

---
+++ Todo.API/Todo.Business/Member/IMemberService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Business.Member
+{
+    public interface IMemberService 
+    {
+        IEnumerable<Model.Member> Get();
+        Model.Member GetSingle(int id);
+        IEnumerable<Model.Member> GetAdult();
+        void Save(Model.Member member);
+        void Delete(int id);
+        void Update(int id, Model.Member member);
+    }
+}
