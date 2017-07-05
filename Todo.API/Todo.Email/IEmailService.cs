8ae2e4aca8af016692143bc0dd003420a07be6a1
Medson de Oliveira Junior <medson.oliveira@fatec.sp.gov.br>
2017-07-05 12:01:15 -0300

    Add interface Email Service

---
+++ Todo.API/Todo.Email/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Email
+{
+    public interface IEmailService
+    {
+        bool Send(Email email);
+    }
+}
