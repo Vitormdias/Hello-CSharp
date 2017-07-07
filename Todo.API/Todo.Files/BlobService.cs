56ba718057361d599c16e87a49c16268475f27b5
Medson de Oliveira Junior <medson.oliveira@fatec.sp.gov.br>
2017-07-06 21:52:16 -0300

    Add Todo.Files

---
+++ Todo.API/Todo.Files/BlobService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace Todo.Files
+{
+    public class BlobService
+    {
+    //    public CloudStorageAccount _storageAccount;
+    //    public CloudBlobContainer container;
+    //    public CloudBlobClient blobClient;
+
+        public async void Init()
+        {
+
+            var storageCredentials = new StorageCredentials("ictestit", "XOgu/B04gLo5fZk4MKMImefGy1pLVH7nlEA8Tlpa37H0Wr2zIYKeG/rjMoI7JtcLw7TZB1hgg8VevQWoDIT0iw==");
+            var cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
+            var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            var container = cloudBlobClient.GetContainerReference("mycontainer");
+            await container.CreateIfNotExistsAsync();
+            var blockBlob = container.GetBlockBlobReference("material01");
+            await blockBlob.UploadFromFileAsync(@".\..\Todo.Files\teste.pdf");
+            var newBlob = container.GetBlockBlobReference("material01");
+            await newBlob.DownloadToFileAsync("./../Todo.Files/testeR.pdf", FileMode.Create);
+        }
+
+    }
+}
---
+++ Todo.API/Todo.Files/Common.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Files
+{
+    public static class Common
+    {
+        public static CloudStorageAccount CreateStorageAccountFromConnectionString()
+        {
+            CloudStorageAccount storageAccount;
+            const string Message = "Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.";
+
+            try
+            {
+                storageAccount = new CloudStorageAccount(new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials("", ""), true);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(Message);
+                Console.ReadLine();
+                throw;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine(Message);
+                Console.ReadLine();
+                throw;
+            }
+
+            return storageAccount;
+        }
+    }
+}
